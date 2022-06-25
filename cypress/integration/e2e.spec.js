995831f14906c55870008414d11c0315d69395a8
vferreirap2 <vferreirap2@gmail.com>
2022-06-25 18:11:42 -0300

    Exercicio de compra na loja Ebac completo

--- cypress/integration/e2e.spec.js
+++ cypress/integration/e2e.spec.js
@@ -1,5 +1,8 @@
 /// <reference types="cypress" />
 
+import dadosEndereco from '../fixtures/endereco.json';
+import dadosLogin from '../fixtures/perfil.json';
+
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
     /*  Como cliente 
         Quero acessar a Loja EBAC 
@@ -10,12 +13,29 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
         E validando minha compra ao final */
 
     beforeEach(() => {
-        cy.visit('/')
+        cy.clearCookies()
+        cy.clearLocalStorage()
     });
 
     it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-        //TODO 
+
+        
+        // login
+        cy.visit('/minha-conta/')
+        cy.login(dadosLogin.usuario, dadosLogin.senha)
+        
+        // compra
+        cy.visit('/produtos')
+        cy.addProdutos('Aero Daily Fitness Tee', 'XS', 'Brown', 4)
+        
+        // informações de entrega
+        cy.detalhesFaturamento()
+
+
+        // finaliza compra
+        cy.finalizarCompra()
+
+        // validar compra bem sucedida
+        cy.contains('Obrigado. Seu pedido foi recebido.')
     });
-
-
 })
